6a8d0daebffcf6e707d7df7aaf616f4f29b11c67
DavidRodera <droderavaras@gmail.com>
2026-01-28 09:25:29 +0100

    En proceso

--- 1ºASIR/ISO/UD5. [ Linux ] Administración Local/script-usuarios-DRV.sh
+++ 1ºASIR/ISO/UD5. [ Linux ] Administración Local/script-usuarios-DRV.sh
@@ -16,15 +16,39 @@ then
 				sleep 1
 				echo ''
 				echo 'NOMBRE	   APELLIDO   CORREO                            USUARIO'
-				echo '======================================================================================================='
+				echo '========================================================================'
 				cut -d"," -f1,2,5,6 $1 | column -t -s ","
-				while IFS=, read -r nombre apellido dni telefono correo usuario
-    				do  
-					echo $usuario
-					sleep 1
+				echo '========================================================================'
+				read -p 'Desea crear estos usuarios [S/N]: ' si_no
+				echo ''
+				if [ $si_no == 'S' ] || [ $si_no == 's' ]
+				then
+					sudo groupadd alumnos_sn 2>/dev/null
+					while IFS=, read -r nombre apellido dni telefono correo usuario
+    					do  
+					sudo useradd $usuario -c "Usuario temporal Academia Sierra Nevada - [$nombre $apellido]" -d /home/aula3/$usuario -m -s /bin/bash -g alumnos_sn -e 2026-01-29 -f 0 2>/dev/null
+					if [ $? -eq 0 ]
+					then
+						echo 'Usuario' $usuario 'CREADO correctamente ✓' 
+						echo ''
+						sleep 0.5
+					else
+						echo 'El usuario' $usuario 'YA EXISTIA'
+						echo ''
+						sleep 0.5
+					fi
 
-    				done < $1
-    				
+    					done < $1
+				else
+					echo -n 'SALIENDO'
+					for i in {1..3}; do
+				    	echo -n '.'
+				    	sleep 0.5
+					done
+					sleep 0.2
+					exit
+
+				fi
 			else
 				echo 'ERROR: El parámetro' $1 'no tiene usuarios.'
 				exit
