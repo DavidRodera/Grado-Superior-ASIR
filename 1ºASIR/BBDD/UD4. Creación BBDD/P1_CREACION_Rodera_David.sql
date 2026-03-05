ca8733627e94b56b788a9d0cb3e7fdf393e6927e
DavidRodera <droderavaras@gmail.com>
2026-03-05 12:55:45 +0100

    ⬆️ ASIR: Subida de nuevos materiales y prácticas

--- 1ºASIR/BBDD/UD4. Creación BBDD/P1_CREACION_Rodera_David.sql
+++ 1ºASIR/BBDD/UD4. Creación BBDD/P1_CREACION_Rodera_David.sql
@@ -2,16 +2,17 @@ create database gestion_universidad;
 use gestion_universidad;
 
 CREATE TABLE facultades (
-    id_facultad INT UNSIGNED AUTO_INCREMENT,
-    codigo VARCHAR(4) UNIQUE NOT NULL,
+    id_facultad TINYINT UNSIGNED AUTO_INCREMENT,
+    codigo CHAR(4) UNIQUE NOT NULL,
     nombre VARCHAR(40) UNIQUE NOT NULL,
     id_decano INT UNSIGNED,
-    CONSTRAINT pk_id_facultad PRIMARY KEY (id_facultad)
+    CONSTRAINT pk_id_facultad PRIMARY KEY (id_facultad),
+    CONSTRAINT chk_codigo_longitud CHECK (LENGTH(codigo) = 4)
 );
 
 CREATE TABLE profesores (
-    id_profesor INT UNSIGNED AUTO_INCREMENT,
-    nif CHAR(9) UNIQUE NOT NULL,
+    id_profesor SMALLINT UNSIGNED AUTO_INCREMENT,
+    nif VARCHAR(9) UNIQUE NOT NULL,
     nombre_completo VARCHAR(40) NOT NULL,
     salario DECIMAL(7 , 2 ) DEFAULT 2000.00,
     id_facultad INT UNSIGNED NOT NULL,
@@ -19,7 +20,8 @@ CREATE TABLE profesores (
     CONSTRAINT fk_id_facultad FOREIGN KEY (id_facultad)
         REFERENCES facultades (id_facultad)
         ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT chk_salario_mayor_cero CHECK (salario > 0)
+    CONSTRAINT chk_salario_mayor_cero CHECK (salario > 0),
+    CONSTRAINT chk_nif_longitud CHECK (LENGTH(nif) = 9)
 );
 
 alter table facultades
@@ -28,7 +30,7 @@ ADD CONSTRAINT fk_id_decano FOREIGN KEY (id_decano)
         ON DELETE RESTRICT ON UPDATE CASCADE;
 
 CREATE TABLE grados (
-    id_grado INT UNSIGNED AUTO_INCREMENT,
+    id_grado SMALLINT UNSIGNED AUTO_INCREMENT,
     nombre VARCHAR(40) UNIQUE NOT NULL,
     id_facultad INT UNSIGNED NOT NULL,
     CONSTRAINT pk_id_grado PRIMARY KEY (id_grado),
@@ -38,17 +40,18 @@ CREATE TABLE grados (
 );
 
 CREATE TABLE asignaturas (
-    id_asignatura INT UNSIGNED AUTO_INCREMENT,
+    id_asignatura SMALLINT UNSIGNED AUTO_INCREMENT,
     codigo_asig VARCHAR(10) UNIQUE NOT NULL,
     nombre VARCHAR(40) NOT NULL,
     creditos DECIMAL(2 , 0 ) DEFAULT 6,
     CONSTRAINT chk_creditos_mayor_igual_tres CHECK (creditos >= 3),
+    CONSTRAINT chk_codigo_asig_max_longitud CHECK (LENGTH(codigo_asig) <= 10),
     CONSTRAINT pk_id_asignatura PRIMARY KEY (id_asignatura)
 );
 
 CREATE TABLE imparten (
-    id_profesor INT UNSIGNED,
-    id_asignatura INT UNSIGNED,
+    id_profesor SMALLINT UNSIGNED,
+    id_asignatura SMALLINT UNSIGNED,
     tipo_grupo ENUM('TEORIA', 'PRACTICA') DEFAULT 'TEORIA',
     PRIMARY KEY (id_profesor , id_asignatura),
     CONSTRAINT fk_id_profesor FOREIGN KEY (id_profesor)
