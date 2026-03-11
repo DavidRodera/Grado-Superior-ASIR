00a1e9d9655effaac49585fb92601f64b523da0d
DavidRodera <droderavaras@gmail.com>
2026-03-11 17:09:22 +0100

    ⬆️ ASIR: Subida de nuevos materiales y prácticas

--- CONFGITHUB/gitpush.sh
+++ CONFGITHUB/gitpush.sh
@@ -6,6 +6,8 @@ ruta=$(find /home/$(whoami) -type d -name "$repositorio" -print -quit)
 cd $ruta
 if [ $? -eq 0 ]
 then
+	git push
+	echo ''
 	git add .
 	git commit -m '⬆️ ASIR: Subida de nuevos materiales y prácticas'
 	git push
